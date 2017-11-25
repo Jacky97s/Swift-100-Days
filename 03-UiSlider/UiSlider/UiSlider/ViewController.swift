d415bd23a9fd1fd15500d356a6d88fcfc362a014
niclin <bboyceo@hotmail.com>
2017-11-25 16:23:43 +0800

    UIslider done

--- 03-UiSlider/UiSlider/UiSlider/ViewController.swift
+++ 03-UiSlider/UiSlider/UiSlider/ViewController.swift
@@ -13,11 +13,18 @@ class ViewController: UIViewController {
     // MARK: - View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
+        helloLabel.textColor = UIColor(hue: 0, saturation: 0, brightness: 0, alpha: CGFloat(slider.value))
     }
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var helloLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
     
     // MARK: - Interactions
 
     @IBAction func dragSlider(_ sender: UISlider) {
+        helloLabel.textColor = UIColor(hue: 0, saturation: 0, brightness: 0, alpha: CGFloat(sender.value))
         
         print("\(sender.value)")
     }
