ebaca59ae7bf59113148e24da9c7bc4b1b1628d3
niclin <bboyceo@hotmail.com>
2017-11-26 00:12:44 +0800

    Add done toolbar

--- 04-TipCalculator/04-TipCalculator/ViewController.swift
+++ 04-TipCalculator/04-TipCalculator/ViewController.swift
@@ -14,9 +14,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         subtotalTextfield.becomeFirstResponder()
+        createToolbar()
     }
     
     // MARK: - Outlets
     @IBOutlet weak var subtotalTextfield: UITextField!
+    
+    // MARK: - Functions
+    func createToolbar() {
+        let toolbar = UIToolbar()
+        toolbar.barStyle = .default
+        toolbar.sizeToFit()
+        
+        // create barbuttonitems
+        
+        let doneButton = UIBarButtonItem(
+            barButtonSystemItem: .done,
+            target: self,
+            action: #selector(dismissKeyboard))
+        
+        let flexibleSpace = UIBarButtonItem(
+            barButtonSystemItem: .flexibleSpace,
+            target: self,
+            action: nil)
+        
+        // add items to toolbar
+        toolbar.setItems([flexibleSpace, doneButton], animated: true)
+        
+        // add toolbar to keyboard
+        subtotalTextfield.inputAccessoryView = toolbar
+    }
+    
+    @objc func dismissKeyboard() {
+        subtotalTextfield.resignFirstResponder()
+    }
 }
 
