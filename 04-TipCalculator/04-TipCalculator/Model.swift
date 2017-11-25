60d788c309c9e4a744905cd523d73748354f763e
niclin <bboyceo@hotmail.com>
2017-11-26 00:25:19 +0800

    Add comment

--- 04-TipCalculator/04-TipCalculator/Model.swift
+++ 04-TipCalculator/04-TipCalculator/Model.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+class Model {
+    
+    // Basic Logic
+    // 1. take input
+    // 2. calculate new values -> rest
+    // 3. return as string for labels
+    
+    // accept subtotal as string
+    // accept tipPercent as Int
+    
+    // convert subtotal to NSDecimal for calculation
+    // convert tipPercent to NSDecimal for calcuation
+    
+    // calculate tipAmount
+    // calculayte totalAmount
+    
+    // convert subtotal to Currency
+    // convert tipAmount to Cuttency
+    // convert total to Currency
+}
