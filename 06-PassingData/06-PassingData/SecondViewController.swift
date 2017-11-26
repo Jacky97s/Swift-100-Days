2d8415e81da6a9148e72340f95642342344598e6
niclin <bboyceo@hotmail.com>
2017-11-26 18:31:39 +0800

    Add second view controller

---
+++ 06-PassingData/06-PassingData/SecondViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecondViewController.swift
+//  06-PassingData
+//
+//  Created by Nic on 2017/11/26.
+//  Copyright © 2017年 Nic. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    // MARK: - View Did Load
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Outlets
+    @IBOutlet weak var messageLabel: UILabel!
+    
+}
