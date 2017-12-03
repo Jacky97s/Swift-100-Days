a3e1e0f28e2a8a47af1cb781cab676d5a6544c56
niclin <bboyceo@hotmail.com>
2017-12-03 23:26:15 +0800

    Add navigation bar UI

--- 09-PullToRefresh/09-PullToRefresh/GroceriesTableViewController.swift
+++ 09-PullToRefresh/09-PullToRefresh/GroceriesTableViewController.swift
@@ -13,12 +13,10 @@ class GroceriesTableViewController: UITableViewController {
     // MARK: - View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add
+            , target: self, action: nil)
     }
     
     // MARK: - Properties
