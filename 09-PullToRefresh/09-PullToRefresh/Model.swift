65a5e7f4b5d4cad28b054cafd7d8b38ab9fc8e1d
niclin <bboyceo@hotmail.com>
2017-12-10 01:08:26 +0800

    Table data can delete

---
+++ 09-PullToRefresh/09-PullToRefresh/Model.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  09-PullToRefresh
+//
+//  Created by Nic on 2017/12/10.
+//  Copyright © 2017年 Nic. All rights reserved.
+//
+
+import Foundation
+
+class Model {
+    var data = ["Milk", "Apples", "Ham", "Eggs"]
+    var cloudData = ["Pancakes", "Orange Juice", "Apples", "Milk", "Apples", "Ham", "Eggs"]
+}
